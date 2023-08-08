e092085903101458715dc5de5e3ea3ced823b05e
Vladyslav Starichenko <vladyslav.starichenko@nure.ua>
2023-08-08 16:17:26 +0200

    Initial commit

---
+++ src/test/java/goldsilver/splunk/demo/SplunkDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package goldsilver.splunk.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SplunkDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
